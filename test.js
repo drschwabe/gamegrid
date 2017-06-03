7d5a1b84073012a6882606049a392155ef4e36c5
Derrick Schwabe <admin@nerdbusiness.com>
2017-06-04 00:16:28 +0900

    Add test to test inserting an enty into a grid.

--- test.js
+++ test.js
@@ -4,10 +4,21 @@ var gg = require('./gg.js'),
 
 test('Grid is created', (t) => {
   t.plan(4)
-  console.log('gg.createGrid(3,3)')
   var grid = gg.createGrid(3,3)
   t.equals(grid.width, 3, 'grid.width is 3')
   t.equals(grid.height, 3, 'grid.height is 3')
   t.ok(_.isArray(grid.enties), 'grid.enties is an array')
   t.equals(grid._id, 'grid_0', 'grid._id is grid_0')
+})
+
+test('Can insert an enty', (t) => {
+  t.plan(3)
+  var grid = gg.createGrid(3,3)
+  gg.insertEnty(grid, {
+    name : 'mushroom', 
+    cell : 3
+  })
+  t.equals(grid.enties[0].name, 'mushroom', "enty's name is correct")
+  t.equals(grid.enties[0].cell, 3, "enty was inserted into the correct cell")
+  t.ok(_.isString(grid.enties[0]._id), "enty has an _id string" )
 })
