f5a205881467c5065d29017cfebecb632b48c177
Derrick Schwabe <admin@nerdbusiness.com>
2019-04-14 21:54:37 -0600

    Create a test to test the output of terminal-grid demo

--- demos/terminal-grid/demo.js
+++ demos/terminal-grid/demo.js
@@ -1,13 +1,20 @@
 var gg = require('../../gg.js')
 const _ = require('underscore')
 
-
 //extend gg with terminal-grid lib:
 gg = require('./lib.js')(gg)
 gg._render = true //< auto rendering
 
+var separator = () => console.log('-------------')
+
 var grid = gg.createGrid(3,3)
 gg._grid = grid
+separator()
 
-gg.insertEnty(grid, 3, 'H')
-gg.move(grid, 'H', 'east')
+gg.insertEnty(3, 'H')
+separator()
+
+gg.move('H', 'east')
+separator()
+
+gg.remove(4)
---
+++ demos/terminal-grid/test.js
@@ -0,0 +1,25 @@
+const test = require('tape')
+const childProcess = require('child_process')
+const _s = require('underscore.string')
+
+test('"Terminal Grid" demo renders as expected', (t) => {
+  t.plan(3)
+
+  //run the demo and collect output as a string:
+  var output = childProcess.execSync('node ./demo.js').toString()
+
+  //in the demo grids are separated with the below string:
+  var grids = output.split('-------------')
+
+  //The demo renders grids to terminal automatically, so during each of
+  //these function calls during the demo (as noted below) a grid is generated...
+
+  //var grid = gg.createGrid(3,3)
+  var firstGridDotCount = _s.count( grids[0], '.')
+  var firstGridEdgeCount = _s.count( grids[0], '[') + _s.count( grids[0], ']')
+
+  t.equals( firstGridDotCount , 9, 'There are 9 dots representing 9 blank cells ' )
+  t.equals( firstGridEdgeCount , 6, 'There are 6 brackets representing 6 edges')
+
+
+})
