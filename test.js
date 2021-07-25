7887008e50f78f7be3ab9379df15487e89bb5d97
Derrick <admin@nerdbusiness.com>
2021-07-25 05:08:17 -0600

    Update test to accommodate for change in 6ccb494d159f0fee449a96795b43079d2585271f

--- test.js
+++ test.js
@@ -3,12 +3,11 @@ var gg = require('./gg.js'),
     _ = require('underscore')
 
 test('Grid is created', (t) => {
-  t.plan(4)
+  t.plan(3)
   var grid = gg.createGrid(3,3)
   t.equals(grid.width, 3, 'grid.width is 3')
   t.equals(grid.height, 3, 'grid.height is 3')
   t.ok(_.isArray(grid.enties), 'grid.enties is an array')
-  t.equals(grid._id, 'grid_0', 'grid._id is grid_0')
 })
 
 test('Can insert an enty', (t) => {
