cb2e72e65410417c8bb7d78ea30581cd94201cc6
Derrick Schwabe <drschwabe@fastmail.com>
2023-07-12 23:28:23 -0600

    Increment to v1 alpha, make the module available again via package.json, explicitly indicate what to publish via npm (files array in package.json), comment in a sourcemap file handy for debugging (sep sourcemap was not working for some reason)

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   // config options
   build : {
+    //sourcemap : 'inline', 
     lib: {
       entry: 'gg.js',
       formats:['es']
